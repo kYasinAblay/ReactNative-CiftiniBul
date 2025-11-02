9aa6e5033d1052b52b9fa027c06aa80197369957
kyasinablay <kamilyasinablay@hotmail.com>
2025-11-02 23:59:00 +0300

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,6 @@
+import React from 'react';
 import { Image, StyleSheet, Platform ,View} from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import MemoryGame from '../memory-game';
 
 export default function HomeScreen() {
