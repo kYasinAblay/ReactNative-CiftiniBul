9a54d7993e641b9ff222365c222fb8e5cb549144
kyasinablay <kamilyasinablay@hotmail.com>
2025-11-02 23:23:21 +0300

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,13 +8,19 @@ import MemoryGame from '../memory-game';
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1 }}>
+   <View style={styles.container}>
       <MemoryGame />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    padding: 20,
+    paddingTop:50,
+    backgroundColor: '#f5f5f5',
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
